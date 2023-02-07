3f6a3f58d9bdcc0d7304a7aad0b8946c42f67ad0
hex <hex@hexair.local>
2023-02-07 14:58:08 +0100

    Updated paths

--- save_data.sh
+++ save_data.sh
@@ -4,6 +4,8 @@ host="my_room" # To use as a filter on influxDB
 # Obtain sensor data
 script="BME280.py"
 script_path=$(dirname $(readlink -f $0))
+log_path="/tmp/tmp"
+tempFile="${log_path}/tempFile"
 res=`/usr/bin/python ${script_path}/${script}`
 temp=`echo ${res} | /usr/bin/awk '{print $1}'`
 presure=`echo ${res} | /usr/bin/awk '{print $2}'`
@@ -15,8 +17,8 @@ time=$time"000000000"
 case $temp in
 	''|*[!0-9.]*) return ;;
 	*)
-		echo "temp_c,host=${host} value=${temp} ${time}" >> ${script_path}/tempFile
-		echo "presure,host=${host} value=${presure} ${time}" >> ${script_path}/tempFile
-		echo "humidity,host=${host} value=${humidity} ${time}" >> ${script_path}/tempFile
+		echo "temp_c,host=${host} value=${temp} ${time}" >> ${tempFile}
+		echo "presure,host=${host} value=${presure} ${time}" >> ${tempFile}
+		echo "humidity,host=${host} value=${humidity} ${time}" >> ${tempFile}
 	;;
 esac
