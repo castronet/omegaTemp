d1f9e6d494844fd387eb3dfb539ffc647326ce6b
hex <hex@hexair.local>
2023-02-07 14:58:29 +0100

    updated paths

--- send_data.sh
+++ send_data.sh
@@ -5,12 +5,13 @@ url="http://your_influx_url:8086/write?db=Your_InfluxDB"
 login=""
 pass=""
 
-script_path=$(dirname $(readlink -f $0))
+#script_path=$(dirname $(readlink -f $0))
+tempFile="/tmp/tmp/tempFile"
 
 # Send the data to influxdb
-res=`/usr/bin/curl -sw '%{http_code}' -u ${login}:${pass} -XPOST "${url}" --data-binary @${script_path}/tempFile`
+res=`/usr/bin/curl -sw '%{http_code}' -u ${login}:${pass} -XPOST "${url}" --data-binary @${tempFile}`
 
 # If the data is sent, the script save the file as compressed file until next success sending
 if [ "x$res" == "x204" ]; then
- /usr/bin/bzip2 -f ${script_path}/tempFile
+ /usr/bin/bzip2 -f ${tempFile}
 fi
